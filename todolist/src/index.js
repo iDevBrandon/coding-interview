0f8506f68e35915aa91c27d28c67f44e8a9bfe17
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-05-19 23:29:21 +0900

    delete to do

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -4,13 +4,28 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
+const addTodo = (text) => {
+  // store.dispatch({ type: "ADD_TODO", text });
+  return {
+    type: "ADD_TODO",
+    text,
+  };
+};
+
+const deleteTodo = (id) => {
+  return {
+    type: "DELETE_TODO",
+    id,
+  };
+};
+
 const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case "ADD_TODO":
       return [...state, { text: action.text, id: Date.now() }];
     case "DELETE_TODO":
-      return [];
+      return state.filter((toDo) => toDo.id !== parseInt(action.id));
     default:
       return state;
   }
@@ -19,33 +34,37 @@ const reducer = (state = [], action) => {
 const store = createStore(reducer);
 store.subscribe(() => console.log(store.getState()));
 
+const dispatchDeleteTodo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteTodo(id));
+};
+
 const paintTodos = () => {
   const toDos = store.getState();
+  ul.innerHTML = "";
   toDos.forEach((toDo) => {
     const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DELETE";
+    btn.addEventListener("click", dispatchDeleteTodo);
     li.id = toDo.id;
     li.innerText = toDo.text;
+    li.appendChild(btn);
     ul.appendChild(li);
   });
 };
 
 store.subscribe(paintTodos);
 
-const addTodo = (text) => {
-  store.dispatch({ type: "ADD_TODO", text });
-};
-
-const createTodo = (toDo) => {
-  const li = document.createElement("li");
-  li.innerText = toDo;
-  ul.appendChild(li);
+const dispatchAddTodo = (text) => {
+  store.dispatch(addTodo(text));
 };
 
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  addTodo(toDo);
+  dispatchAddTodo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
