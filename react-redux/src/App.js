072d1f8977a3b8b2e23e23ebc83050b29131fbe9
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-05-20 01:45:26 +0900

    react redux

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,21 +1,7 @@
-import React, { useState } from "react";
-import AddNumberRoot from "./components/AddNumberRoot";
-import "./App.css";
-import DisplayNumberRoot from "./components/DisplayNumberRoot";
+import React from "react";
 
 function App() {
-  const [number, setNumber] = useState(0);
-  return (
-    <div>
-      <h1>Root</h1>
-      <AddNumberRoot
-        onClick={function (size) {
-          setNumber({ number: number + size });
-        }}
-      ></AddNumberRoot>
-      <DisplayNumberRoot number={number}></DisplayNumberRoot>
-    </div>
-  );
+  return <div>hi</div>;
 }
 
 export default App;
