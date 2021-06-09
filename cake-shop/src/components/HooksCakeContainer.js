d58c778009971531939d3f7dd109b0512ee73885
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-06-09 19:31:27 +0900

    useSelector Hook

---
+++ cake-shop/src/components/HooksCakeContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const HooksCakeContainer = () => {
+  const numOfCakes = useSelector((state) => state.numOfCakes);
+  return (
+    <div>
+      <h2>Num of cakes - {numOfCakes}</h2>
+      <button>Buy cake</button>
+    </div>
+  );
+};
+
+export default HooksCakeContainer;
