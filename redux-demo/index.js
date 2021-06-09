debeb37723bb5466dbc6c254dec16151a14ed87c
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-06-09 12:27:20 +0900

    Combine Reducers

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,7 +1,13 @@
+// Store
+// es6 import
+const redux = require("redux");
+const createStore = redux.createStore;
+
 // Action is the plain object here inside return
 // Action Creator a function that returns an action
 
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
 
 function buyCake() {
   return {
@@ -10,14 +16,51 @@ function buyCake() {
   };
 }
 
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+  };
+}
+
 // Reducer
 // (prevState, action) => return a new state
 
-const initialState = {
+// const initialState = {
+//   numOfCakes: 10,
+//   numOfIceCreams: 20,
+// };
+
+// split a state and reducer for multiple reducers
+const initialCakeState = {
   numOfCakes: 10,
 };
 
-const reducer = (state = initialState, action) => {
+const initialIceCreamState = {
+  numOfIceCreams: 20,
+};
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case BUY_CAKE:
+//       return {
+//         // object might contain more than one property
+//         ...state,
+//         numOfCakes: state.numOfCakes - 1,
+//       };
+
+//     case BUY_ICECREAM:
+//       return {
+//         // object might contain more than one property
+//         ...state,
+//         numOfIceCreams: state.numOfIceCreams - 1,
+//       };
+
+//     default:
+//       return state;
+//   }
+// };
+
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
@@ -30,3 +73,34 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        // object might contain more than one property
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+// Holds application state
+const store = createStore(reducer);
+// Allows access to state via getState()
+console.log("Initial state", store.getState());
+// Registers listneres via subscribe(listner)
+// Handles unregistering of listners
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated state", store.getState())
+);
+// Allows state to be updated via dispatch(action)
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+unsubscribe();
