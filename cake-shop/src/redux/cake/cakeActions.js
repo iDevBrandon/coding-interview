e2b2c3bb0d72f29d0228d0d78e2e008fe9f76a5a
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-06-09 15:44:55 +0900

    Create actions/reducers

---
+++ cake-shop/src/redux/cake/cakeActions.js
@@ -0,0 +1,10 @@
+// action creator
+import { BUY_CAKE } from "./cakeTypes";
+
+export const buyCake = () => {
+  return {
+    type: BUY_CAKE,
+  };
+};
+
+ 
---
+++ cake-shop/src/redux/cake/cakeReducer.js
@@ -0,0 +1,20 @@
+// reducer is a function that accepts state and action in param and return state
+import { BUY_CAKE } from "./cakeTypes";
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const cakeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+export default cakeReducer;
