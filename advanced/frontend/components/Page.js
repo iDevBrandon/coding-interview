0d1ed393efa24dc5f1c40a7971652e003ab46b3c
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-07-01 03:18:30 +0900

    Header and nav component

--- advanced/frontend/components/Page.js
+++ advanced/frontend/components/Page.js
@@ -1,8 +1,10 @@
 import PropTypes from "prop-types";
+import Header from "./Header";
 
 export default function Page({ children, cool }) {
   return (
     <div>
+      <Header></Header>
       <h2>I am the page component</h2>
       {children}
     </div>
