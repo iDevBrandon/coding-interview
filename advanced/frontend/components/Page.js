3c9fee33e82d8e8461101df5f2fffac4b1a43601
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-07-01 15:03:17 +0900

    global styles

--- advanced/frontend/components/Page.js
+++ advanced/frontend/components/Page.js
@@ -1,12 +1,62 @@
 import PropTypes from "prop-types";
 import Header from "./Header";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+@font-face {
+  font-family: 'radnika_next';
+  src: url('/static/radnikanext-medium-webfont.woff2')
+  format('woff2');
+  font-style: normal;
+}
+  html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3a3a3a;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(----lightGray);
+    --offWhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+  }
+
+  body {
+    font-family: 'radnika_next', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--black);
+  }
+
+  button {
+    font-family: 'radnika_next', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 export default function Page({ children, cool }) {
   return (
     <div>
-      <Header></Header>
-      <h2>I am the page component</h2>
-      {children}
+      <GlobalStyles />
+      <Header />
+      <InnerStyles>{children}</InnerStyles>
     </div>
   );
 }
