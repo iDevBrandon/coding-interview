e49f187f9286d792fb22c88780ec6dcbd8a91728
Brandon Ha <idevbrandon@gmail.com>
2021-08-12 01:02:19 +0900

    before catching an error

--- stack/Redux/raul/src/components/AddBook/AddBook.js
+++ stack/Redux/raul/src/components/AddBook/AddBook.js
@@ -1,23 +1,44 @@
+import { nanoid } from "nanoid";
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addBook } from "../../store/actions/bookActions";
 
 const AddBook = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [rating, setRating] = useState("5");
 
+  const dispatch = useDispatch();
+
   const addBookHandler = (e) => {
     e.preventDefault();
+    dispatch(
+      addBook({
+        title,
+        author,
+        rating,
+        id: nanoid(),
+      })
+    );
   };
 
   return (
     <form className="add-book">
       <div>
         <label htmlFor="title">Title</label>
-        <input type="text" value={title} onChange={(e) => e.target.value} />
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="author">Author</label>
-        <input type="text" value={author} onChange={(e) => e.target.value} />
+        <input
+          type="text"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="rating">Rating</label>
@@ -26,7 +47,7 @@ const AddBook = () => {
           min="1"
           max="10"
           value={rating}
-          onChange={(e) => e.target.value}
+          onChange={(e) => setRating(e.target.value)}
         />
       </div>
       <button onClick={addBookHandler}>Add book</button>
--- stack/Redux/raul/src/components/Library/Library.js
+++ stack/Redux/raul/src/components/Library/Library.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { deleteBook } from "../../store/actions/bookActions";
 
 const Library = () => {
+  const dispatch = useDispatch();
+
   const deleteBookHandler = (id) => {
     // dispatch a delete book action
+    dispatch(deleteBook(id));
   };
+
+  const books = useSelector((state) => state.books.books);
+
+  const booksTable = books.map((book) => {
+    return (
+      <tr>
+        <td>{book.title}</td>
+        <td>{book.author}</td>
+        <td>{book.rating}</td>
+        <td>
+          <button className="delete" onClick={() => deleteBookHandler(book.id)}>
+            x
+          </button>
+        </td>
+      </tr>
+    );
+  });
+
   return (
     <div className="library">
       <h2>Library</h2>
@@ -14,16 +37,7 @@ const Library = () => {
           <th>Rating</th>
           <th></th>
         </tr>
-        <tr>
-          <td>Dune</td>
-          <td>F. Herbert</td>
-          <td>10</td>
-          <td>
-            <button className="delete" onClick={deleteBookHandler}>
-              x
-            </button>
-          </td>
-        </tr>
+        {booksTable}
       </table>
     </div>
   );
