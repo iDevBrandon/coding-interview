64283ce247bfead1ea8bd3904b7a93bd4cf0fd16
Brandon Ha <idevbrandon@gmail.com>
2021-08-23 17:39:59 +0900

    Fetching data from API

--- Stack/Redux/todo/src/redux/todoSlice.js
+++ Stack/Redux/todo/src/redux/todoSlice.js
@@ -1,12 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+// createAsyncThunk is a function that returns a function. Thunk is a new action that can be dispatched.
+// when the response is completed from the api call, the thunk will be dispatched.
+
+export const getTodosAsync = createAsyncThunk(
+  "todos/getTodosAsync",
+  async () => {
+    const response = await fetch("http://localhost:7000/todos");
+    if (response.ok) {
+      const todos = await response.json();
+      return { todos };
+    }
+  }
+);
 
 const todoSlice = createSlice({
   name: "todos",
-  initialState: [
-    { id: 1, title: "Microsoft", completed: false },
-    { id: 2, title: "Google", completed: true },
-    { id: 3, title: "Mastercard", completed: false },
-  ],
+  initialState: [],
 
   reducers: {
     addTodo: (state, action) => {
@@ -26,6 +36,17 @@ const todoSlice = createSlice({
 
     deleteTodo: (state, action) => {
       return state.filter((todo) => todo.id !== action.payload.id);
+    },
+  },
+
+  extraReducers: {
+    // thunk will dispatch a number of actions based on the response from the api call.
+    [getTodosAsync.pending]: (state, action) => {
+      console.log("Data is being fetched from API");
+    },
+    [getTodosAsync.fulfilled]: (state, action) => {
+      console.log("Here your data is!");
+      return action.payload.todos;
     },
   },
 });
