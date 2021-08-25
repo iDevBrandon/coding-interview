3d9feec2e47e3af0033d8161eca29c032ca221f3
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-08-26 02:21:08 +0900

    type dispatching

--- stack/Redux/ts-redux/src/redux/reducers/bankReducer.ts
+++ stack/Redux/ts-redux/src/redux/reducers/bankReducer.ts
@@ -1,28 +1,15 @@
+import { ActionType } from "../action-types";
+import { Action } from "../actions/index";
+
 const initialState = 0;
-
-interface DepositAction {
-  type: "deposit";
-  payload: number;
-}
-
-interface WithdrawAction {
-  type: "withdraw";
-  payload: number;
-}
-
-interface BankruptAction {
-  type: "bankrupt";
-}
-
-type Action = DepositAction | WithdrawAction | BankruptAction;
 
 const reducer = (state: number = initialState, action: Action) => {
   switch (action.type) {
-    case "deposit":
+    case ActionType.DEPOSIT:
       return state + action.payload;
-    case "withdraw":
+    case ActionType.WITHDRAW:
       return state - action.payload;
-    case "bankrupt":
+    case ActionType.BANKRUPT:
       return 0;
     default:
       return state;
