5ce33d190fd6f3407c59a492b1baa777d7803080
Brandon Ha <idevbrandon@gmail.com>
2021-09-08 01:39:26 +0900

    my attempt

--- PS/inflearn/sec6_stack_queue/1.js
+++ PS/inflearn/sec6_stack_queue/1.js
@@ -2,18 +2,32 @@ function solution(s) {
   let answer = "YES";
   let stack = [];
 
-  for (let x of s) {
-    if (x === "(") {
-      stack.push(x);
+  //   for (let x of s) {
+  //     if (x === "(") {
+  //       stack.push(x);
+  //     } else {
+  //       if (stack.length === 0) answer = "NO";
+  //       else {
+  //         stack.pop();
+  //       }
+  //     }
+  //   }
+
+  s.split("").forEach((x) => stack.push(x));
+
+  for (let i = 0; i <= stack.length; i++) {
+    if (stack[i] === "(") {
+      stack.pop();
     } else {
-      if (stack.length === 0) answer = "NO";
-      else {
-        stack.pop();
+      stack.pop();
+      if (stack.length === 0) {
+        return "NO";
       }
     }
   }
+  if (stack.length > 0) return "NO";
   return answer;
 }
 
-let input = "(())";
+let input = "((())";
 console.log(solution(input));
