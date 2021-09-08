59c77748ac2d617bc61be7dd08501b69a52721a4
Brandon Ha <idevbrandon@gmail.com>
2021-09-08 17:22:34 +0900

    stack Q2

---
+++ PS/inflearn/sec6_stack_queue/2.js
@@ -0,0 +1,17 @@
+function solution(s) {
+  let answer;
+  let stack = [];
+
+  for(let x of s) {
+    if(x === ')') {
+      while(stack.pop() !== '(');
+    }
+    else stack.push(x)
+  }
+
+  answer = stack.join('')
+  return answer;
+}
+
+let input = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
+console.log(solution(input)); // EFLM
