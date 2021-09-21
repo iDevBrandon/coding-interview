a0810f4478ff0428bfee9debc36080406dbb67e6
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-09-22 00:02:53 +0900

    Code for header grouping

--- stack/React/react-table/src/components/columns.js
+++ stack/React/react-table/src/components/columns.js
@@ -30,3 +30,48 @@ export const COLUMNS = [
     accessor: "phone",
   },
 ];
+
+export const GROUPED_COLUMNS = [
+  {
+    Header: "id",
+    Footer: "id",
+    accessor: "id",
+  },
+  {
+    Header: "Name",
+    Footer: "Name",
+    columns: [
+      {
+        Header: "First Name",
+        Footer: "First Name",
+        accessor: "first_name",
+      },
+      {
+        Header: "Last Name",
+        Footer: "Last Name",
+        accessor: "last_name",
+      },
+    ],
+  },
+  {
+    Header: "Info",
+    Footer: "Info",
+    columns: [
+      {
+        Header: "Date of Birth",
+        Footer: "Date of Birth",
+        accessor: "date_of_birth",
+      },
+      {
+        Header: "Country",
+        Footer: "Country",
+        accessor: "country",
+      },
+      {
+        Header: "Phone",
+        Footer: "Phone",
+        accessor: "phone",
+      },
+    ],
+  },
+];
