a98e5098f4f3f67d24c8370a2a6bcf2be216fd94
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-09-21 23:29:55 +0900

    basic table setup

---
+++ stack/React/react-table/src/components/BasicTable.js
@@ -0,0 +1,72 @@
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { COLUMNS } from "./columns";
+import "./table.css";
+
+const BasicTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  // create a table instance
+  const tableInstance = useTable({
+    columns: columns,
+    data: data,
+  });
+
+  // give us from the table package to create it easily
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+  } = tableInstance;
+
+  // define the table structure
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return (
+                  <td {...cell.getCellProps()}>
+                    {
+                      // Render the cell contents
+                      cell.render("Cell")
+                    }
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+      <tfoot>
+        {footerGroups.map((footerGroup) => {
+          <tr {...footerGroup.getFooterGroupProps()}>
+            {footerGroup.headers.map((column) => (
+              <td {...column.getFooterProps}>{column.render("Footer")}</td>
+            ))}
+          </tr>;
+        })}
+      </tfoot>
+    </table>
+  );
+};
+
+export default BasicTable;
