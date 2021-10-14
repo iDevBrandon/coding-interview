a5f3c5db51d0c051b54ba1b6d118563a4e615c83
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-10-15 00:16:31 +0900

    최적화

--- stack/vlpt/35_redux_basic/src/containers/CounterContainer.js
+++ stack/vlpt/35_redux_basic/src/containers/CounterContainer.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Counter from "../components/Counter";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
 import { decrease, increase, setDiff } from "../modules/counter";
 
 const CounterContainer = () => {
-  const { number, diff } = useSelector((state) => ({
-    number: state.counter.number,
-    diff: state.counter.diff,
-  }));
+  const { number, diff } = useSelector(
+    (state) => ({
+      number: state.counter.number,
+      diff: state.counter.diff,
+    }),
+    shallowEqual
+  );
+
+  //   const number = useSelector((state) => state.counter.number);
+  //   const diff = useSelector((state) => state.counter.diff);
 
   const dispatch = useDispatch();
 
@@ -21,7 +27,7 @@ const CounterContainer = () => {
       diff={diff}
       onIncrease={onIncrease}
       onDecrease={onDecrease}
-      onSetDiff={onSetDiff} 
+      onSetDiff={onSetDiff}
     />
   );
 };
