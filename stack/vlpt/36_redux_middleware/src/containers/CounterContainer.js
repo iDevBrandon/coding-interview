d4209ca8a9deb2ec19c44ccf8430f31d5cd61d17
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-10-15 00:41:52 +0900

    typo

--- stack/vlpt/36_redux_middleware/src/containers/CounterContainer.js
+++ stack/vlpt/36_redux_middleware/src/containers/CounterContainer.js
@@ -1,24 +1,22 @@
 import React from "react";
 import Counter from "../components/Counter";
 import { useSelector, useDispatch } from "react-redux";
-import { decrease, increase } from "../modules/counter";
+import { increase, decrease } from "../modules/counter";
 
-const CounterContainer = () => {
-  const number = useSelector((state) => state.counter.number);
+function CounterContainer() {
+  const number = useSelector((state) => state.counter);
   const dispatch = useDispatch();
 
-  const onIncrease = () => dispatch(increase);
-  const onDecrease = () => dispatch(decrease);
+  const onIncrease = () => {
+    dispatch(increase());
+  };
+  const onDecrease = () => {
+    dispatch(decrease());
+  };
 
   return (
-    <div>
-      <Counter
-        number={number}
-        onIncrease={onIncrease}
-        onDecrease={onDecrease}
-      />
-    </div>
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
-};
+}
 
 export default CounterContainer;
