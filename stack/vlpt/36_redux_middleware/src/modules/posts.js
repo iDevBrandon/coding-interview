a0161e024695ccd6fbdb5106ee761288581f8cdd
Brandon Ha <idevbrandon@gmail.com>
2021-10-16 16:19:52 +0900

    라우터 연동

--- stack/vlpt/36_redux_middleware/src/modules/posts.js
+++ stack/vlpt/36_redux_middleware/src/modules/posts.js
@@ -8,7 +8,7 @@ import {
   createPromiseSaga,
   createPromiseSagaById,
 } from "../lib/asyncUtils";
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, getContext } from "redux-saga/effects";
 // 액션 타입을 선언
 // 한 요청당 세 개를 만들어야함.
 
@@ -21,6 +21,7 @@ const GET_POST_SUCCESS = "GET_POST_SUCCESS";
 const GET_POST_ERROR = "GET_POST_ERROR";
 
 const CLEAR_POST = "CLEAR_POST";
+const GO_TO_HOME = "GO_TO_HOME";
 
 // thunk 함수를 생성합니다
 // thunk 함수 내부에서는 시작할 때, 성공했을 떄, 실패했을 때 다른 액션을 디스패치합니다
@@ -71,17 +72,20 @@ export const getPost = (id) => ({ type: GET_POST, payload: id, meta: id });
 
 const getPostsSaga = createPromiseSaga(GET_POSTS, postsAPI.getPosts);
 const getPostSaga = createPromiseSagaById(GET_POST, postsAPI.getPostById);
+
+function* goToHomeSaga() {
+  const history = yield getContext("history");
+  history.push("/");
+}
+
 // 액션을 모니터를 하는 작업
 export function* postsSaga() {
   yield takeEvery(GET_POSTS, getPostsSaga);
   yield takeEvery(GET_POST, getPostSaga);
+  yield takeEvery(GO_TO_HOME, goToHomeSaga);
 }
 
-export const goToHome =
-  () =>
-  (dispatch, getState, { history }) => {
-    history.push("/");
-  };
+export const goToHome = () => ({ type: GO_TO_HOME });
 
 export const clearPost = () => ({ type: CLEAR_POST });
 // 초기상태 선언 & 리듀서 작성
