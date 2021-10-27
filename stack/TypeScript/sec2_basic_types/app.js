febabf5df6d4d9626e2ddefc23ba4afa317a1b52
Brandon Ha <idevbrandon@gmail.com>
2021-10-28 00:23:22 +0900

    14. working with other types

--- stack/TypeScript/sec2_basic_types/app.js
+++ stack/TypeScript/sec2_basic_types/app.js
@@ -1,7 +1,12 @@
-function add(num1, num2) {
-    return num1 + num2;
+function add(num1, num2, showResult) {
+    if (showResult) {
+        console.log(num1 + num2);
+    }
+    else {
+        return num1 + num2;
+    }
 }
 var num1 = 5;
 var num2 = 2.4;
-var result = add(num1, num2);
-console.log(result);
+var showResult = true;
+add(num1, num2, showResult);
--- stack/TypeScript/sec2_basic_types/app.ts
+++ stack/TypeScript/sec2_basic_types/app.ts
@@ -1,9 +1,13 @@
-function add(num1: number, num2: number): number {
-  return num1 + num2;
+function add(num1: number, num2: number, showResult: boolean) {
+  if (showResult) {
+    console.log(num1 + num2);
+  } else {
+    return num1 + num2;
+  }
 }
 
 const num1 = 5;
 const num2 = 2.4;
+const showResult = true;
 
-const result = add(num1, num2);
-console.log(result);
+add(num1, num2, showResult);
