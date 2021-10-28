c1e19c97d11637c2a7f6403ddf14e14b83d2962e
Brandon Ha <idevbrandon@gmail.com>
2021-10-28 16:04:15 +0900

    27. Fuctions as Types

--- stack/TypeScript/sec2_basic_types/union-alias.ts
+++ stack/TypeScript/sec2_basic_types/union-alias.ts
@@ -1,3 +1,11 @@
-function result(num: number): void {
-  console.log(num);
+function add(num: number) {
+  return num;
 }
+
+function printReulst(num1: number, num2: number): void {
+  console.log(num1 + num2);
+}
+
+let combinedValues: (a: number, b: number) => number;
+
+combinedValues = add;
