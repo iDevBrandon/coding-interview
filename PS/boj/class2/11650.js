8ad5590570a2ecfa9f4aca05779289a8e1144cd7
Brandon Ha <idevbrandon@gmail.com>
2021-11-02 16:52:22 +0900

    compare

--- PS/boj/class2/11650.js
+++ PS/boj/class2/11650.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 // For local test
-
+// const input = ["7", "1 -1", "3 3", "1 -2", "1 1", "3 -2", "-2 1", "-3 1"];
 const N = input.shift();
 const coordsArr = input.map((coords) =>
   coords.split(" ").map((nums) => parseInt(nums))
@@ -25,3 +25,26 @@ console.log(results);
 
 ////////////////////////////////////////////////////
 // Mine
+
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+const inputLeng = Number(input[0]); //5
+let N = input.shift();
+const coordsArr = input.map((coords) =>
+  coords.split(" ").map((num) => parseInt(num))
+);
+
+let answer = "";
+// // result[i][0] in order
+let range = coordsArr
+  .sort((a, b) => {
+    if (a[0] !== b[0]) {
+      return a[0] - b[0];
+    }
+    return a[1] - b[1];
+  })
+  .forEach((coords) => {
+    answer += `${coords[0]} ${coords[1]}\n`;
+  });
+
+console.log(answer);
