93fdcf74dd3d0bdcfa149b9e70af8bcc5f39fad5
Brandon Ha <idevbrandon@gmail.com>
2021-11-02 16:50:01 +0900

    up

--- PS/boj/class2/11650.js
+++ PS/boj/class2/11650.js
@@ -25,24 +25,3 @@ console.log(results);
 
 ////////////////////////////////////////////////////
 // Mine
-
-const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-
-let N = input.shift();
-const coordsArr = input.map((coords) =>
-  coords.split(" ").map((num) => parseInt(num))
-);
-
-let results = "";
-coordsArr
-  .sort((a, b) => {
-    if (a[0] !== b[0]) {
-      return a[0] - b[0];
-    }
-    return a[1] - b[1];
-  })
-  .forEach((coords) => {
-    results += `${coords[0]} ${coords[1]}\n`;
-  });
-
-console.log(results);
