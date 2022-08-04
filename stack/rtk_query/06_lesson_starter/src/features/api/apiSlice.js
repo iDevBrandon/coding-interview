127666570e9e1d93563e387d550a343686b580a4
Brandon Ha <idevbrandon@gmail.com>
2022-08-04 09:02:53 +0900

    RTK Query TODO version

---
+++ stack/rtk_query/06_lesson_starter/src/features/api/apiSlice.js
@@ -0,0 +1,50 @@
+// interact with API
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+  tagTypes: ["Todos"],
+  endpoints: (builder) => ({
+    getTodos: builder.query({
+      query: () => "/todos",
+      transformResponse: (res) => res.sort((a, b) => b.id - a.id),
+      providesTags: ["Todos"],
+    }),
+
+    addTodo: builder.mutation({
+      query: (todo) => ({
+        url: "/todos",
+        method: "POST",
+        body: todo,
+      }),
+      invalidatesTags: ["Todos"],
+    }),
+
+    updateTodo: builder.mutation({
+      query: (todo) => ({
+        url: `/todos/${todo.id}`,
+        method: "PUT",
+        body: todo,
+      }),
+      invalidatesTags: ["Todos"],
+    }),
+
+    deleteTodo: builder.mutation({
+      query: ({ id }) => ({
+        url: `/todos/${id}`,
+        method: "DELETE",
+        body: id,
+      }),
+      invalidatesTags: ["Todos"],
+    }),
+  }),
+});
+
+// RTK Query create a custom hook
+export const {
+  useGetTodosQuery,
+  useAddTodoMutation,
+  useUpdateTodoMutation,
+  useDeleteTodoMutation,
+} = apiSlice;
