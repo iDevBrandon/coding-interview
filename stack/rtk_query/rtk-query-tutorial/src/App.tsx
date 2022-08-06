e23234413d597ee4c11dd3411c92d811da1f1dd3
Brandon Ha <idevbrandon@gmail.com>
2022-08-06 17:48:24 +0900

    query all contacts & each contact

--- stack/rtk_query/rtk-query-tutorial/src/App.tsx
+++ stack/rtk_query/rtk-query-tutorial/src/App.tsx
@@ -1,8 +1,34 @@
 import React from "react";
 import "./App.css";
+import { useContactQuery, useContactsQuery } from "./services/contactsApi";
 
 function App() {
-  return <div className="App">asdf</div>;
+  const { data, error, isLoading, isFetching, isSuccess } = useContactsQuery();
+  return (
+    <div className="App">
+      <h1>React redux RTK Query Tutorial</h1>
+      {isLoading && <p>Loading...</p>}
+      {isFetching && <p>Fetching...</p>}
+      {error && <p>Error happened! </p>}
+      {isSuccess && (
+        <div>
+          {data?.map((contact) => (
+            <div className="data" key={contact.id}>
+              <span>{contact.name}</span>
+              <span>
+                <ContactDetail id={contact.id} />
+              </span>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
+
+export const ContactDetail = ({ id }: { id: string }) => {
+  const { data } = useContactQuery(id);
+  return <pre>{JSON.stringify(data, undefined, 2)}</pre>;
+};
 
 export default App;
--- stack/rtk_query/rtk-query-tutorial/src/services/contactsApi.tsx
+++ stack/rtk_query/rtk-query-tutorial/src/services/contactsApi.tsx
@@ -8,7 +8,12 @@ export const contactsApi = createApi({
     contacts: builder.query<Contact[], void>({
       query: () => "/contacts",
     }),
+
+    // fetch a single contact
+    contact: builder.query<Contact, string>({
+      query: (id) => `/contacts/${id}`,
+    }),
   }),
 });
 
-export const { useContactsQuery } = contactsApi;
+export const { useContactsQuery, useContactQuery } = contactsApi;
