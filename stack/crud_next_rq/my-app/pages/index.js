a8683a0aeba7eb6c11629bc5cb1b437eb710e706
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2022-10-22 18:27:19 +0900

    [#7] - Collapsible Form

--- stack/crud_next_rq/my-app/pages/index.js
+++ stack/crud_next_rq/my-app/pages/index.js
@@ -2,8 +2,14 @@ import Head from "next/head";
 import { BiUserPlus } from "react-icons/bi";
 import Form from "../components/form";
 import Table from "../components/table";
+import { useState } from "react";
 
 export default function Home() {
+  const [visible, setVisible] = useState(false);
+
+  const showFormHandler = () => {
+    setVisible(!visible);
+  };
   return (
     <section>
       <Head>
@@ -19,7 +25,10 @@ export default function Home() {
 
         <div className="container mx-auto flex justify-between py-5 border-b">
           <div className="left flex gap-3">
-            <button className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 hover:text-gray-800">
+            <button
+              onClick={showFormHandler}
+              className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 hover:text-gray-800"
+            >
               Add Employee{" "}
               <span className="px-1">
                 <BiUserPlus size={23}></BiUserPlus>
@@ -29,9 +38,8 @@ export default function Home() {
         </div>
 
         {/* Collapsable form */}
-        <div className="container mx-auto py-5">
-          <Form></Form>
-        </div>
+
+        {visible ? <Form></Form> : <></>}
 
         {/* table */}
         <div className="container mx-auto">
