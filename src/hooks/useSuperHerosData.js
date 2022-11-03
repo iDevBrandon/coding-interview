0c0b274971c85e6551b1dad33c918a3fc216091d
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2022-11-03 15:40:04 +0900

    Missing part

--- src/hooks/useSuperHerosData.js
+++ src/hooks/useSuperHerosData.js
@@ -5,8 +5,8 @@ const fetchSuperHeros = () => {
   return axios.get("http://localhost:4000/superheroes");
 };
 
-const addSuperHero = () => {
-  return axios.post("http://localhost:4000/superheroes");
+const addSuperHero = (hero) => {
+  return axios.post("http://localhost:4000/superheroes", hero);
 };
 
 export const useSuperHerosData = (onSuccess, onError) => {
