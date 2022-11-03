7587478b4d1ec72688b4cd42c6a04e0094382386
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2022-11-03 16:40:41 +0900

    Typo

--- src/util/axios-utils.js
+++ src/util/axios-utils.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-const client = axios.create({ baseUrl: "http://localhost:4000" });
+const client = axios.create({ baseURL: "http://localhost:4000" });
 
 export const request = ({ ...options }) => {
-  client.defaults.headers.common.Authorization = `Bearer Token`;
+  client.defaults.headers.common.Authorization = `Bearer token`;
+
   const onSuccess = (response) => response;
   const onError = (error) => {
+    // optionaly catch errors and add additional logging here
     return error;
   };
 
