f81a30b946ffdaaae5cb535018b8c6820254242e
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2022-11-06 03:11:38 +0900

    Typo!

--- crud/database/controller.js
+++ crud/database/controller.js
@@ -2,7 +2,7 @@ import Users from "../model/user";
 
 export async function getUsers(req, res) {
   try {
-    const users = Users.find({});
+    const users = await Users.find({});
     if (!users) return res.status(404).json({ error: "Data not found!" });
     res.status(200).json(users);
   } catch (error) {
