7741e400fcfb35d16ed027c8ee2bff71cd5ca3a1
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2022-11-06 00:23:13 +0900

    style: display success& error component

--- crud/components/form.js
+++ crud/components/form.js
@@ -1,5 +1,6 @@
 import { useReducer } from "react";
 import { BiPlus } from "react-icons/bi";
+import Success from "./success";
 
 const formReducer = (state, event) => {
   return {
@@ -15,7 +16,13 @@ export default function Form() {
     event.preventDefault();
 
     console.log(formData);
+    if (Object.keys(formData).length == 0)
+      return console.log("Don't have Form Data");
   };
+
+  if (Object.keys(formData).length > 0) {
+    return <Success message={"Data added"} />;
+  }
   return (
     <form className="grid lg:grid-cols-2 w-4/6 gap-4" onSubmit={handleSubmit}>
       <div className="input-type">
