859b682a6ce2718d26e7e5270d1b7cf8ab2084cd
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2022-11-08 15:27:56 +0900

    Getting started with login & register page

---
+++ nextauth/pages/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Head from "next/head";
+
+const Login = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>Login
+    </div>
+  );
+};
+
+export default Login;
---
+++ nextauth/pages/register.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+const Register = () => {
+  return (
+    <div>
+      <Head>
+        <title>Register</title>
+      </Head>
+      Register
+    </div>
+  );
+};
+
+export default Register;
