da26d983bf4c2bb0dbcfb5b9208270a5e4bb81b9
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2022-11-09 03:53:05 +0900

    Protected route with signout

--- nextauth/pages/index.js
+++ nextauth/pages/index.js
@@ -3,11 +3,15 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
-import { useSession, signOut } from "next-auth/react";
+import { useSession, signOut, getSession } from "next-auth/react";
 
 export default function Home() {
   // const [session, setSession] = useState(true);
   const { data: session } = useSession();
+
+  function handleSignout() {
+    signOut();
+  }
 
   return (
     <div className={styles.container}>
@@ -17,7 +21,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {session ? User({ session }) : Guest()}
+      {session ? User({ session,handleSignout }) : Guest()}
     </div>
   );
 }
@@ -40,7 +44,7 @@ function Guest() {
 }
 
 // Authorised user
-function User({ session }) {
+function User({ session, handleSignout }) {
   return (
     <main className="container mx-auto text-center py-20">
       <h3 className="text-4xl font-bold">Authorize User Homepage</h3>
@@ -51,7 +55,10 @@ function User({ session }) {
       </div>
 
       <div className="flex justify-center">
-        <button className="mt-5 px-10 py-1 rounded-sm bg-indigo-500">
+        <button
+          onClick={handleSignout}
+          className="mt-5 px-10 py-1 rounded-sm bg-indigo-500"
+        >
           Sign Out
         </button>
       </div>
@@ -65,4 +72,21 @@ function User({ session }) {
       </div>
     </main>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
 }
