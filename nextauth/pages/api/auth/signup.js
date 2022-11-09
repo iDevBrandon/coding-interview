7fb4702f45dcb23b30c7fc652a1867b55209f7c7
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2022-11-10 05:28:06 +0900

    typo

--- nextauth/pages/api/auth/signup.js
+++ nextauth/pages/api/auth/signup.js
@@ -1,18 +1,20 @@
 import connectMongo from "../../../database/conn";
 import Users from "../../../model/Schema";
 import { hash } from "bcryptjs";
-export default async function handle(req, res) {
-  connectMongo().catch((error) => res.json({ error: "Connection error" }));
 
-  if (req.body === "POST") {
+export default async function handler(req, res) {
+  connectMongo().catch((error) => res.json({ error: "Connection Failed...!" }));
+
+  // only post method is accepted
+  if (req.method === "POST") {
     if (!req.body)
-      return res.status(404).json({ error: "Don't have form data" });
-
+      return res.status(404).json({ error: "Don't have form data...!" });
     const { username, email, password } = req.body;
 
-    const existingUser = await Users.findOne({ email });
-    if (existingUser)
-      return res.status(422).json({ message: "User already existed" });
+    // check duplicate users
+    const checkexisting = await Users.findOne({ email });
+    if (checkexisting)
+      return res.status(422).json({ message: "User Already Exists...!" });
 
     // hash password
     Users.create(
@@ -23,8 +25,8 @@ export default async function handle(req, res) {
       }
     );
   } else {
-    return res
+    res
       .status(500)
-      .json({ message: "HTTP method not valid only POST accepted" });
+      .json({ message: "HTTP method not valid only POST Accepted" });
   }
 }
