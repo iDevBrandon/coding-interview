68d9c90ed3d068ab7149e6b1edd8b2f027ddc3c7
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2022-11-10 05:27:06 +0900

    register a user

--- nextauth/pages/api/auth/signup.js
+++ nextauth/pages/api/auth/signup.js
@@ -1,3 +1,30 @@
+import connectMongo from "../../../database/conn";
+import Users from "../../../model/Schema";
+import { hash } from "bcryptjs";
 export default async function handle(req, res) {
-  res.json({ message: "req" });
+  connectMongo().catch((error) => res.json({ error: "Connection error" }));
+
+  if (req.body === "POST") {
+    if (!req.body)
+      return res.status(404).json({ error: "Don't have form data" });
+
+    const { username, email, password } = req.body;
+
+    const existingUser = await Users.findOne({ email });
+    if (existingUser)
+      return res.status(422).json({ message: "User already existed" });
+
+    // hash password
+    Users.create(
+      { username, email, password: await hash(password, 12) },
+      function (err, data) {
+        if (err) return res.status(404).json({ err });
+        res.status(201).json({ status: true, user: data });
+      }
+    );
+  } else {
+    return res
+      .status(500)
+      .json({ message: "HTTP method not valid only POST accepted" });
+  }
 }
