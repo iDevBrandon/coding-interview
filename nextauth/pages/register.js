47eeb2858f4c5389ada3d8ed28e4523e54a20fa3
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2022-11-10 03:34:53 +0900

    Register Formik

--- nextauth/pages/register.js
+++ nextauth/pages/register.js
@@ -6,9 +6,23 @@ import styles from "../styles/Form.module.css";
 import Image from "next/image";
 import { HiAtSymbol, HiFingerPrint, HiOutlineUser } from "react-icons/hi";
 import { useState } from "react";
+import { useFormik } from "formik";
 
 const Register = () => {
   const [show, setShow] = useState({ password: false, cpassword: false });
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+      cpassword: "",
+    },
+    onSubmit,
+  });
+
+  async function onSubmit(values) {
+    console.log(values);
+  }
 
   return (
     <Layout>
@@ -25,13 +39,14 @@ const Register = () => {
           </p>
         </div>
         {/* form */}
-        <form className="flex flex-col gap-5">
+        <form className="flex flex-col gap-5" onSubmit={formik.handleSubmit}>
           <div className={styles.input_group}>
             <input
               type="text"
-              name="Username"
+              name="username"
               placeholder="Username"
               className={styles.input_text}
+              {...formik.getFieldProps("username")}
             />
             <span className="icon flex items-center px-4">
               <HiOutlineUser size={25} />
@@ -43,6 +58,7 @@ const Register = () => {
               name="email"
               placeholder="Email"
               className={styles.input_text}
+              {...formik.getFieldProps("email")}
             />
             <span className="icon flex items-center px-4">
               <HiAtSymbol size={25} />
@@ -54,6 +70,7 @@ const Register = () => {
               name="password"
               placeholder="password"
               className={styles.input_text}
+              {...formik.getFieldProps("password")}
             />
             <span
               className="icon flex items-center px-4"
@@ -69,6 +86,7 @@ const Register = () => {
               name="cpassword"
               placeholder="Confirm Password"
               className={styles.input_text}
+              {...formik.getFieldProps("cpassword")}
             />
             <span
               className="icon flex items-center px-4"
