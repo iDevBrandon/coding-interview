8411f3801a757567f25db2451b4a76557c1c0702
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2022-11-10 04:31:06 +0900

    formik Validation

--- nextauth/pages/login.js
+++ nextauth/pages/login.js
@@ -7,6 +7,7 @@ import { HiAtSymbol, HiFingerPrint } from "react-icons/hi";
 import { useState } from "react";
 import { signIn, signOut } from "next-auth/react";
 import { useFormik } from "formik";
+import login_validate from "../lib/validate";
 
 const Login = () => {
   const [show, setShow] = useState(false);
@@ -16,6 +17,7 @@ const Login = () => {
       password: "",
     },
     onSubmit,
+    validate: login_validate,
   });
 
   async function onSubmit(values) {
@@ -55,6 +57,11 @@ const Login = () => {
               <HiAtSymbol size={25} />
             </span>
           </div>
+          {formik.errors.email && formik.touched.email ? (
+            <span className="text-rose-500">{formik.errors.email}</span>
+          ) : (
+            <></>
+          )}
           <div className={styles.input_group}>
             <input
               type={`${show ? "text" : "password"}`}
@@ -70,6 +77,11 @@ const Login = () => {
               <HiFingerPrint size={25} />
             </span>
           </div>
+          {formik.errors.password && formik.touched.password ? (
+            <span className="text-rose-500">{formik.errors.password}</span>
+          ) : (
+            <></>
+          )}
 
           <div className="input-button">
             <button type="submit" className={styles.button}>
