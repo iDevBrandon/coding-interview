0603bfce6d80263c811faff115b969f2cb8b137e
Brandon Ha <idevbrandon@gmail.com>
2023-03-21 20:48:11 +0900

    implement query

---
+++ stack/graphql/yalco/dbtester.js
@@ -0,0 +1,4 @@
+// equipment를 반환하는 쿼리 만들어두기
+
+const database = require("./database");
+console.log(database.supplies);
--- stack/graphql/yalco/index.js
+++ stack/graphql/yalco/index.js
@@ -3,6 +3,9 @@ const { ApolloServer, gql } = require("apollo-server");
 const typeDefs = gql`
   type Query {
     teams: [Team]
+    team(id: Int): Team
+    equipments: [Equipment]
+    supplies: [Supply]
   }
   type Team {
     id: Int
@@ -12,11 +15,30 @@ const typeDefs = gql`
     mascot: String
     cleaning_duty: String
     project: String
+    supplies: [Supply]
+  }
+  type Equipment {
+    id: String
+    userd_by: String
+    count: Int
+    new_or_used: String
+  }
+  type Supply {
+    id: String
+    team: String
   }
 `;
 const resolvers = {
   Query: {
-    teams: () => database.teams,
+    teams: (parent, args, context, info) =>
+      database.teams.map((team) => {
+        team.supplies = database.supplies.filter((supply) => {
+          return supply.team === team.id;
+        });
+        return team;
+      }),
+    equipments: () => database.equipments,
+    supplies: () => database.supplies,
   },
 };
 const server = new ApolloServer({ typeDefs, resolvers });
