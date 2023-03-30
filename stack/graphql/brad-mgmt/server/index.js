760bb809326042c0f5bb1c834888dc4de1703c70
Brandon Ha <idevbrandon@gmail.com>
2023-03-30 19:02:06 +0900

    feat : set client types & queries

--- stack/graphql/brad-mgmt/server/index.js
+++ stack/graphql/brad-mgmt/server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 
@@ -10,8 +10,16 @@ app.use(
   "/graphql",
   graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === " development",
+    graphiql: process.env.NODE_ENV === "development",
   })
 );
+
+// app.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     schema,
+//     graphiql: process.env.NODE_ENV === "development",
+//   })
+// );
 
 app.listen(port, console.log(`Server started on port ${port}`));
